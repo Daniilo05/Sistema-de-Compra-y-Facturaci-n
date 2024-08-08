e74d4fc137b5f4c197c1d16d7212e4ffdcc142dc
Danilo Muñoz <danilovmunoz120@gmail.com>
2024-08-07 20:16:18 -0500

    template-listar/ Menu option

--- inv/models.py
+++ inv/models.py
@@ -5,5 +5,14 @@ from bases.models import ClaseModelo
 class Categoria(ClaseModelo):
     descripcion = models.CharField(
         max_length=100,
-        help_text=
+        help_text='Descripcion de Categoria',
+        unique=True,
     )
+    
+    def __str__(self):
+        return '{}'.format(self.descripcion)
+
+    class Meta:
+        verbose_name_plural = 'Categorias'
+        
+    
