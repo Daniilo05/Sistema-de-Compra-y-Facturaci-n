ad3479f00c62f9ad050c303debae3f7d6d57bbc0
Danilo Muñoz <danilovmunoz120@gmail.com>
2024-08-14 02:06:09 -0500

    Update/Delete

--- inv/views.py
+++ inv/views.py
@@ -14,6 +14,7 @@ class CategoriaView(LoginRequiredMixin, generic.ListView):
 class CategoriaNew(LoginRequiredMixin, generic.CreateView):
     model = Categoria
     template_name = "inv/categoria_form.html"
+    context_object_name = "obj"
     form_class = CategoriaForm
     success_url = reverse_lazy("inv:categoria_list")
     login_url = "bases:login"
@@ -22,10 +23,24 @@ class CategoriaNew(LoginRequiredMixin, generic.CreateView):
         form.instance.uc = self.request.user
         return super().form_valid(form)
     
+class CategoriaEdit(LoginRequiredMixin, generic.UpdateView):
+    model = Categoria
+    template_name = "inv/categoria_form.html"
+    context_object_name = "obj"
+    form_class = CategoriaForm
+    success_url = reverse_lazy("inv:categoria_list")
+    login_url = "bases:login"
 
+    def form_valid(self, form):
+        form.instance.um = self.request.user.id
+        return super().form_valid(form)
 
-
-
-
+class CategoriaDelete(LoginRequiredMixin, generic.DeleteView):
+    model = Categoria
+    template_name = "inv/categoria_delete.html"
+    context_object_name = "obj"
+    success_url = reverse_lazy("inv:categoria_list")
+    
+    
     
     
