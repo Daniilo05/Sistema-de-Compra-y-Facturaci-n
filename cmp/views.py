344bfbe53dde1cad44d7bac1bd2f98b8a8d5bd6d
Danilo Muñoz <danilovmunoz120@gmail.com>
2024-08-28 21:13:59 -0500

    Refactorized Proveedor templates and views: Fixed syntax errors, improved message handling, and enhanced table rendering with DataTables

--- cmp/views.py
+++ cmp/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 from .models import Proveedor
 from .forms import ProveedorForm
+
+class BasesCreateView(LoginRequiredMixin, generic.CreateView):
+    login_url = 'base:login'
+
+    def form_valid(self, form):
+        form.instance.uc = self.request.user
+        return super().form_valid(form)
+    
+class BasesEditView(LoginRequiredMixin, generic.UpdateView):
+    login_url = 'base:login'
+    
+    def form_valid(self, form):
+        form.instance.um = self.request.user.id
+        return super().form_valid(form)
+    
 
 class ProveedorView(LoginRequiredMixin, generic.ListView):
     model = Proveedor
@@ -11,37 +27,30 @@ class ProveedorView(LoginRequiredMixin, generic.ListView):
     context_object_name = 'obj'
     login_url = ('bases:login')
     
-class ProveedorNew(LoginRequiredMixin, generic.CreateView):
+class ProveedorNew(BasesCreateView):
     model = Proveedor
     template_name = 'cmp/proveedor_form.html'
     context_object_name = 'obj'
     form_class = ProveedorForm
     success_url = reverse_lazy('cmp:proveedor_list')
     login_url = ('bases:login')
-    
-    def form_valid(self, form):
-        form.instance.uc = self.request.user
-        return super().form_valid(form)
-    
-class ProveedorEdit(LoginRequiredMixin, generic.UpdateView):
+        
+class ProveedorEdit(BasesEditView):
     model =  Proveedor
     template_name = 'cmp/proveedor_form.html'
     context_object_name = 'obj'
     form_class = ProveedorForm
     success_url = reverse_lazy('cmp:proveedor_list')
     login_url = ('bases:login')
-    
-    def form_valid(self, form):
-        form.instance.um = self.request.user.id
-        return super().form_valid(form)
-    
+      
+      
 def proveedor_inactivar(request,id):
     proveedor = get_object_or_404(Proveedor,pk=id)
     
     if request.method=='POST':
         proveedor.estado = False
         proveedor.save()
-        contexto={'obj':'OK'}
+        messages.success(request, "Proveedor inactivado exitosamente.")
         return redirect('cmp:proveedor_list')
         
     return render(request, 'cmp/inactivar_prv.html', {'obj': proveedor})
